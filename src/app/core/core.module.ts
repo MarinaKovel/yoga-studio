4dec9c906fd19e467e1eafbc6b88b00f7379b73b
Marina Kovel <m.kovel@softteco.com>
2025-01-10 14:37:57 +0300

    feat: add footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,16 +4,17 @@ import { HeaderComponent } from './components/header/header.component';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatButton } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
     MatToolbar,
     RouterModule,
     MatButton,
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, FooterComponent]
 })
 export class CoreModule { }
