5d2a23e574200bb9c08de518cf4c6efbcdb20d3c
Marina Kovel <m.kovel@softteco.com>
2025-01-20 15:19:27 +0300

    refactor: team section on home page

--- src/app/home/components/team-section/team-section.component.ts
+++ src/app/home/components/team-section/team-section.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Trainer } from '../../../interfaces/trainer.interface';
-import { TRAINERS } from '../../../share/constants';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import { Router } from '@angular/router';
+import { FetchDataService } from '../../../share/services/fetch-data.service';
 
 @Component({
   selector: 'app-team-section',
@@ -10,11 +10,15 @@ import { Router } from '@angular/router';
   styleUrl: './team-section.component.scss'
 })
 export class TeamSectionComponent {
-  trainers: Trainer[] = TRAINERS;
+  trainers: Trainer[] = [];
 
   isTablet = false;
 
-  constructor(private breakpointService: BreakpointObserver, private router: Router) {}
+  constructor(
+    private breakpointService: BreakpointObserver, 
+    private router: Router,
+    private fetchDataService: FetchDataService,
+  ) {}
 
   ngOnInit() {
     this.breakpointService
@@ -26,9 +30,22 @@ export class TeamSectionComponent {
           this.isTablet = true;
         }
       })
+
+    this.loadTeam();
   }
 
   redirectToAbout() {
     this.router.navigate(['/about']);
+  }
+
+  private loadTeam() {
+      this.fetchDataService.fetchData('?limit=6&page=1').subscribe(
+      (response) => {
+        this.trainers = response
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    )
   }
 }
